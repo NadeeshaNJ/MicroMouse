cf16e0ec1b76a9957cf73d336babc8030acb8a63
Nadeesha Jayamanne <nadeeshajayamanne@protonmail.com>
2025-08-11 22:42:55 +0530

    minor changes

--- lib/RobotNavigatorV2/prev src/RobotNavigatorV2.cpp
+++ lib/RobotNavigatorV2/prev src/RobotNavigatorV2.cpp
@@ -4,119 +4,124 @@ RobotNavigatorV2::RobotNavigatorV2(MotorPIDbyNJ* left, MotorPIDbyNJ* right) {
     leftMotor = left;
     rightMotor = right;
 }
-
-void RobotNavigatorV2::resetEncoders() {
-    leftMotor->resetEncoder();
-    rightMotor->resetEncoder();
-}
-
-int RobotNavigatorV2::wallPID(){
-    float wallError = (sensorDistances[0] - sensorDistances[3]);
-    long currentTime = micros();
-    float deltaTime = ((float)(currentTime - previousTime)) / 1.0e6;    
-    if (deltaTime <= 0.000001) deltaTime = 0.000001;     
-    
-    // Calculate derivative
-    float derivative = (wallError - previousError) / deltaTime;
-    
-    // Calculate integral
-    sumWallError += wallError * deltaTime;
-    sumWallError = constrain(sumWallError, -300, 300);  // Clamp integral
-
-    // Calculate PID output
-    float wallCorrection = wallKp * wallError + wallKi * sumWallError + wallKd * diffWallError;
-    //directionL = (u < 0) ? -1 : 1;
-    //directionR = (u < 0) ? -1 : 1;
-    //directionR = directionL = 1;
-    //speedL = constrain((255 - 5*wallCorrection),0,255); //speed = constrain(abs(255 - 0.1*u),0,255); 
-    //speedR = constrain((255 + 5*wallCorrection),0,255); //speed = constrain(abs(255 - 0.1*u),0,255); 
-
-    previousError = wallError;
-    previousTime = currentTime;
-    return wallCorrection;
-}
-void RobotNavigatorV2::reachTargets(long targetLeft, long targetRight) {
-    leftMotorSpeed = leftMotor->reachTarget(targetLeft);
-    rightMotorSpeed = rightMotor->reachTarget(targetRight);
-    
-}
-void RobotNavigatorV2::moveForward() {
-    Serial.println("Moving Forward");
-    
-    resetEncoders();
-    int wallCorrection = wallPID();
-    reachTargets(4000, 4000);
-    speedL = constrain((leftMotorSpeed - 5*wallCorrection),0,255);
-    speedR = constrain((rightMotorSpeed + 5*wallCorrection),0,255);
-    leftMotor->setDirection(1);
-    rightMotor->setDirection(1);
-    leftMotor->setSpeed(speedL);
-    rightMotor->setSpeed(speedR);
-    
-}
-void RobotNavigatorV2::turnLeft() {
-    Serial.println("Turning Left");
-    resetEncoders();
-    leftMotor->setDirection(1);
-    rightMotor->setDirection(1);
-    leftMotor->reachTarget(400);
-    rightMotor->reachTarget(4000);
-}
-void RobotNavigatorV2::turnRight() {
-    Serial.println("Turning Right");
-    resetEncoders();
-    leftMotor->setDirection(1);
-    rightMotor->setDirection(1);
-    leftMotor->reachTarget(4000);
-    rightMotor->reachTarget(400);
-}
 void RobotNavigatorV2::go(int& facingDirection, int direction) {
+    
     if (direction == -1) return;
     int diff = (facingDirection - direction + 4) % 4;
     
     if (diff == 1) turnLeft();
     else if (diff == 2) turnAround();
-    else if (diff == 3) turnRight();    
-
-    moveForward();
-
-    updatePosition(row, col, facingDirection, direction);
+    else if (diff == 3) turnRight();
+    else moveForward();
 }
-// void RobotNavigatorV2::calculatePID(long error) {
-//     long currentTime = micros();
-//     float deltaTime = ((float)(currentTime - previousTime)) / 1.0e6;    
-//     if (deltaTime <= 0.000001) deltaTime = 0.000001;     
+void RobotNavigatorV2::resetEncoders() {
+    leftMotor->resetEncoder();
+    rightMotor->resetEncoder();
+}
+void RobotNavigatorV2::getEncoderPID() {
+    leftEncoderPID = leftMotor->calculateEncoderPID();
+    rightEncoderPID = rightMotor->calculateEncoderPID();
+}
+void RobotNavigatorV2::getAnglePID() {
+    imuYaw = imu->getYaw();
+}
+void RobotNavigatorV2::setTargets(long targetLeft, long targetRight) {
+    leftMotor->setTarget(targetLeft);
+    rightMotor->setTarget(targetRight);
+}
+int RobotNavigatorV2::calculateWallPID(std::vector<int> sensorDistances) {
+    float wallError = (sensorDistances[0] - sensorDistances[4]);
     
-//     // Calculate derivative
-//     float derivative = (error - previousError) / deltaTime;
+    long currentTime = micros();
+    float deltaTime = ((float)(currentTime - previousSensorTime)) / 1.0e6;    
+    if (deltaTime <= 0.000001) deltaTime = 0.000001;     
     
-//     // Calculate integral
-//     integral += error * deltaTime;
-//     integral = constrain(integral, -300, 300);  // Clamp integral
+    // Calculate derivative
+    float derivative = (wallError - previousSensorError) / deltaTime;
     
-//     // Calculate PID output
-//     float u = kp * error + ki * integral + kd * derivative;
-//     //directionL = (u < 0) ? -1 : 1;
-//     //directionR = (u < 0) ? -1 : 1;
-//     directionR = directionL = 1;
-//     speedL = constrain((255 - 5*u),0,255); //speed = constrain(abs(255 - 0.1*u),0,255); 
-//     speedR = constrain((255 + 5*u),0,255); //speed = constrain(abs(255 - 0.1*u),0,255); 
-//     // Update previous values
-//   //   if (directionL == 1) {
-//   //   analogWrite(25, speedL);
-//   //   analogWrite(26, 0);
-//   // } else {
-//   //   analogWrite(25, 0);
-//   //   analogWrite(26, speedL);
-//   // }
-//   // if (directionR == 1) {
-//   //   analogWrite(14, speedR);
-//   //   analogWrite(27, 0);
-//   // } else {
-//   //   analogWrite(14, 0);
-//   //   analogWrite(27, speedR);
-//   // }
-//     previousError = error;
-//     previousTime = currentTime;
+    // Calculate integral
+    integralWallError += wallError * deltaTime;
+    integralWallError = constrain(integralWallError, -300, 300);  // Clamp integral
 
-// }
+    // Calculate PID output
+    float wallPID = wallKp * wallError + wallKi * integralWallError + wallKd * derivative;
+
+    previousSensorError = wallError;
+    previousSensorTime = currentTime;
+
+    return wallPID;
+}
+
+void RobotNavigatorV2::moveForward() {
+    if (!moving) {
+        Serial.println("Moving Forward");
+        resetEncoders();
+        setTargets(192,192); //192 mm
+        moving = true;
+        cellDone = false;
+    }
+    
+    getEncoderPID();
+    speedL = constrain((leftEncoderPID - 5*calculateWallPID(sensorDistances)),-255,255);
+    speedR = constrain((rightEncoderPID + 5*calculateWallPID(sensorDistances)),-255,255);
+    if(!leftMotor->checkDone()) leftMotor->runMotor(speedL);
+    if(!rightMotor->checkDone()) rightMotor->runMotor(speedR);
+    if(leftMotor->checkDone() && rightMotor->checkDone()) {
+        cellDone = true;
+        moving = false; // ready for next move
+    }
+}
+void RobotNavigatorV2::turnLeft() {
+    if(!moving) {
+        Serial.println("Turning Left");
+        resetEncoders();
+        imu->setTargetYaw(imu->getYaw() + 90); // Adjust target yaw for left turn
+        moving = true;
+        cellDone = false;
+    }
+
+    speedL = constrain((0 - imu->calculateAnglePID()),-255,255);
+    speedR = constrain((0 + imu->calculateAnglePID()),-255,255);
+    if(!leftMotor->checkDone()) leftMotor->runMotor(speedL);
+    if(!rightMotor->checkDone()) rightMotor->runMotor(speedR);
+    if(leftMotor->checkDone() && rightMotor->checkDone()) {
+        cellDone = true; 
+        moving = false; // ready for next move
+    }
+}
+void RobotNavigatorV2::turnRight() {
+    if(!moving) {
+        Serial.println("Turning Right");
+        resetEncoders();
+        imu->setTargetYaw(imu->getYaw() - 90); // Adjust target yaw for right turn
+        moving = true;
+        cellDone = false;
+    }
+
+    speedL = constrain((0 - imu->calculateAnglePID()),-255,255);
+    speedR = constrain((0 + imu->calculateAnglePID()),-255,255);
+    if(!leftMotor->checkDone()) leftMotor->runMotor(speedL);
+    if(!rightMotor->checkDone()) rightMotor->runMotor(speedR);
+    if(leftMotor->checkDone() && rightMotor->checkDone()) {
+        cellDone = true; 
+        moving = false; // ready for next move
+    }
+}
+void RobotNavigatorV2::turnAround() {
+    if(!moving) {
+        Serial.println("Turning Around");
+        resetEncoders();
+        imu->setTargetYaw(imu->getYaw() + 180); // Adjust target yaw for 180 turn
+        moving = true;
+        cellDone = false;
+    }
+
+    speedL = constrain((0 - imu->calculateAnglePID()),-255,255);
+    speedR = constrain((0 + imu->calculateAnglePID()),-255,255);
+    if(!leftMotor->checkDone()) leftMotor->runMotor(speedL);
+    if(!rightMotor->checkDone()) rightMotor->runMotor(speedR);
+    if(leftMotor->checkDone() && rightMotor->checkDone()) {
+        cellDone = true; 
+        moving = false; // ready for next move
+    }
+}
--- lib/RobotNavigatorV2/prev src/RobotNavigatorV2.h
+++ lib/RobotNavigatorV2/prev src/RobotNavigatorV2.h
@@ -4,51 +4,73 @@
 #include <Arduino.h>
 #include <MotorPIDbyNJ.h>
 #include <vector>
+#include <GyroPID.h>
+#include <VL6180XManagerV2.h>
 
 class RobotNavigatorV2 {
 private:
     MotorPIDbyNJ* leftMotor;
     MotorPIDbyNJ* rightMotor;
+    GyroPID* imu;
+
+    std::array<int, 5> xshutPins = {32, 17, 16, 15, 4};
+    std::array<int, 5> sensorCorrections = {0, 6, 16, 43, 26};  // mm to subtract from each sensor
+    VL6180XManagerV2 sensorGroup;
 
     int row;
     int col;
     int direction; // 0 = North, 1 = East, 2 = South, 3 = West
     int facingDirection;
-    float integral;
-    float sumWallError; // Integral of wall error for PID control
-    float diffWallError; // Derivative of wall error for PID control
-    float kp, ki, kd;
+    float integralEncoderError;
+    float integralWallError;
     float directionL, directionR;
     float speedL, speedR;
-    long previousTime;
-    float previousError;
-    std::vector<int> sensorDistances;
+    long previousSensorTime;
+    float previousSensorError;
     float wallKp = 0.1; // Proportional gain for wall following
     float wallKi = 0.0; // Integral gain for wall following
     float wallKd = 0.05; // Derivative gain for wall following
 
+
+    std::vector<int> sensorDistances;
+
+    float imuYaw;
+    float targetYaw; //in degrees
+    float angleKp = 0.1; // Proportional gain for angle PID
+    float angleKi = 0.0; // Integral gain for angle PID
+    float angleKd = 0.05; // Derivative gain for angle PID
+
+    
     int leftMotorSpeed;
     int rightMotorSpeed;
+
+    int leftEncoderPID = 0;
+    int rightEncoderPID = 0;
+
+    int encoderPID;
 
 
 public:
     RobotNavigatorV2(MotorPIDbyNJ* left, MotorPIDbyNJ* right);
-
+    // Add constructor declaration for initialization
+    // RobotNavigatorV2(MotorPIDbyNJ* left, MotorPIDbyNJ* right, 
+    //                  std::array<int, 5> xshutPins = {32, 17, 16, 15, 4}, 
+    //                  std::array<int, 5> sensorCorrections = {0, 6, 16, 43, 26});
+    
+    bool cellDone = true; // Flag to check if the cell is done processing
+    bool moving = false; // Flag to check if the robot is currently moving
+    
     void resetEncoders();         // Initialize starting move
-    void calculatePID(long error);
-    int wallPID();            // Calculate PID control values
+    int calculateWallPID(std::vector<int> sensorDistances);
+    void getEncoderPID();
+    void getAnglePID();           // Get angle PID from GyroPID
+    void setTargets(long targetLeft, long targetRight); // Set targets for both motors
     void go(int& facingDirection, int direction); // Move in a given global direction
     void moveForward();  // Move forward in the current direction
     void moveBackward(); // Move backward in the current direction (not implemented)
     void turnLeft();
     void turnRight();
     void turnAround();
-
-    void reachTargets(long targetLeft, long targetRight);
-
-    void updatePosition(int& row, int& col, int& facingDirection, int direction);
-
-    bool isIdle(); // checks if both motors are done
 };
 
 #endif
